4c4d5d54a451def41cdc89c97bf2d7a7f2c8dc03
mio-mia <alice74105208@gmail.com>
2025-03-07 18:01:46 +0900

    일단 밀어보자,,

--- js/index.js
+++ js/index.js
@@ -1,63 +1,32 @@
-// By https://vuelog.dev
-const MIN_WIDTH = 50;
-const MAX_WIDTH = MIN_WIDTH * 1.8;
-const STEP = (MAX_WIDTH - MIN_WIDTH) * 0.05;
+const icons = document.querySelectorAll(".ico");
 
-let aniID = null;
-const dock = document.querySelector(".dock");
-
-const updateWidth = function (nextWidths) {
-  window.cancelAnimationFrame(aniID);
-
-  aniID = null;
-
-  let isAllDone = true;
-  let newWidth = 0;
-  const items = document.querySelectorAll(".item");
-  for (let i = 0; i < items.length; i++) {
-    const currWidth = items[i].getBoundingClientRect().width;
-    const goalWidth = nextWidths[i];
-    if (goalWidth < currWidth) {
-      newWidth = Math.max(currWidth - STEP, goalWidth);
-      isAllDone = false;
-    } else if (goalWidth > currWidth) {
-      newWidth = Math.min(currWidth + STEP, goalWidth);
-      isAllDone = false;
-    } else {
-      newWidth = goalWidth;
-    }
-    items[i].style.width = newWidth + "px";
-  }
-
-  // 다시 애니메이션 추가
-  if (!isAllDone) {
-    aniID = window.requestAnimationFrame(() => {
-      updateWidth(nextWidths);
-    });
-  }
+const resetIcons = () => {
+  icons.forEach((item) => {
+    item.style.transform = "scale(1) translateY(0px)";
+  });
 };
 
-dock.addEventListener("mousemove", function (e) {
-  const dockTop = e.target.getBoundingClientRect().top;
-  const y = e.clientY - dockTop;
-
-  const nextWidths = [];
-  const items = document.querySelectorAll(".item");
-  for (const item of items) {
-    const rect = item.getBoundingClientRect();
-    const center = rect.top - dockTop + rect.height / 2;
-
-    const dist = Math.abs(center - y);
-    nextWidths.push(Math.max(MAX_WIDTH - dist / 4, MIN_WIDTH));
-  }
-  updateWidth(nextWidths);
+icons.forEach((item, index) => {
+  item.addEventListener("mouseover", () => focus(index));
+  item.addEventListener("mouseleave", resetIcons);
 });
 
-dock.addEventListener("mouseleave", function (e) {
-  const items = document.querySelectorAll(".item");
-  const nextWidths = [];
-  for (const item of items) {
-    nextWidths.push(MIN_WIDTH);
-  }
-  updateWidth(nextWidths);
-});
+const focus = (index) => {
+  resetIcons();
+  const transformations = [
+    { idx: index - 2, scale: 1.05, translateY: 0 },
+    { idx: index - 1, scale: 1.1, translateY: -6 },
+    { idx: index, scale: 1.3, translateY: -10 },
+    { idx: index + 1, scale: 1.1, translateY: -6 },
+    { idx: index + 2, scale: 1.05, translateY: 0 }
+  ];
+
+  transformations.forEach(({ idx, scale, translateY }) => {
+    if (icons[idx]) {
+      console.log(scale);
+      icons[
+        idx
+      ].style.transform = `scale(${scale}) translateY(${translateY}px)`;
+    }
+  });
+};
